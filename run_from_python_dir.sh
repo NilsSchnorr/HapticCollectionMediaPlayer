1cbd3f71c99b0778ff6e8152dc81fd46ae953592
NilsSchnorr <97969202+NilsSchnorr@users.noreply.github.com>
2025-07-26 16:06:25 +0200

    cleanup old stuff

--- run_from_python_dir.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# Script to run the NFC system from the python directory
-# This matches the working example's environment
-
-cd "$(dirname "$0")/python"
-echo "Changed to python directory: $(pwd)"
-echo "Running NFC web server..."
-python3 ../nfc_web_server.py
